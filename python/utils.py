afa0f2255ba1ecfe306049741a43c595f47032da
Sam Korn <korn94sam@gmail.com>
2019-08-25 14:48:50 -0400

    new login commands and filtering based on tags

--- python/utils.py
+++ python/utils.py
@@ -2,13 +2,25 @@ from balena import Balena
 from balena.exceptions import RequestError
 from pprint import pprint
 import sys
-
+import os
 import requests
+import code
 
 
 balena = Balena()
 
-
+def loginWithToken():
+	if not balena.auth.is_logged_in():
+		try:
+			token = os.environ["BALENA_TOKEN"]
+		except KeyError:
+			print("Error: Create Environment Variable: BALENA_TOKEN so we can log in!")
+			sys.exit()
+		# print(token)
+		balena.auth.login_with_token(token)
+		print(balena.auth.who_am_i() )
+	else:
+		print("Aleady Logged in as %s ... Continuing!" % balena.auth.who_am_i() )
 
 def selectAppList():
 	apps = balena.models.application.get_all()
@@ -44,7 +56,17 @@ def getSerialNumbers(device_list):
 			if env['name'] == "SERIAL_NUM":
 				print("%s\t%s" % (uuid,env['value']) )
 
+def getReleaseID():
+	# Filter list of dictionaries
+	# list(filter(lambda d: d['tag_key'] in ['TEST'],tags))
+	# Filter on name and value:
+	# list(filter( lambda d: d['value'] in ['1'], filter(lambda d: d['tag_key'] in ['BLAH'],tags)))
+	pass
+
 if __name__ == '__main__':
+	loginWithToken()
 	app_id = selectAppList()
 	dev_list = getDeviceList(app_id)
 	getSerialNumbers(dev_list)
+
+	code.interact(local = locals())
